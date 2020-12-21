bc9f3ff7c2b565ccd382302d3ffd193ecad5c31d
chevalmuscle <me@marcantoinejean.com>
2020-12-21 18:20:54 -0500

    finished simple calculator

--- main.go
+++ main.go
@@ -1,15 +1,31 @@
 package main
 
 import (
+	"strconv"
 	"syscall/js"
 )
 
 func add(this js.Value, i []js.Value) interface{} {
-	return js.ValueOf(i[0].Int() + i[1].Int())
+	value1 := js.Global().Get("document").Call("getElementById", i[0].String()).Get("value").String()
+	value2 := js.Global().Get("document").Call("getElementById", i[1].String()).Get("value").String()
+
+	int1, _ := strconv.Atoi(value1)
+	int2, _ := strconv.Atoi(value2)
+
+	js.Global().Get("document").Call("getElementById", i[2].String()).Set("value", int1+int2)
+
+	return int1 + int2
 }
 
 func subtract(this js.Value, i []js.Value) interface{} {
-	return js.ValueOf(i[0].Int() - i[1].Int())
+	value1 := js.Global().Get("document").Call("getElementById", i[0].String()).Get("value").String()
+	value2 := js.Global().Get("document").Call("getElementById", i[1].String()).Get("value").String()
+
+	int1, _ := strconv.Atoi(value1)
+	int2, _ := strconv.Atoi(value2)
+	js.Global().Get("document").Call("getElementById", i[2].String()).Set("value", int1-int2)
+
+	return int1 - int2
 }
 
 func registerCallbacks() {
